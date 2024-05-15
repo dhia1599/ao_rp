08bbf74460825b131fda56a83f5b39123d32a1fb
Mohamed Dhia Ben Ahmed <benahmed.meddhia@gmail.com>
2024-05-16 00:14:20 +0200

    set design for login component

--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-import { Box, TextField, Typography } from '@mui/material';
-import Button from '@mui/material/Button';
 import { LoginContainer, LoginContent, LoginFooter, LoginHeader } from '../../components/styled/login';
 import { useTranslation } from 'react-i18next';
 import { useLoginMutation } from '../../services/authApi';
@@ -11,8 +9,6 @@ import { signin, signout } from '../../slices/authSlice';
 import Locale from '../../components/locale/Locale';
 
 function Login() {
-  const { t } = useTranslation(['translation']);
-
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
 
@@ -48,15 +44,15 @@ function Login() {
 
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <h2 className="text-center text-3xl font-extrabold text-gray-900">Login</h2>
-      </div>
-
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+        <div
+          className="bg-white py-10 px-8 shadow sm:rounded-lg sm:px-10"
+          style={{ background: 'linear-gradient(to bottom, #6bb5ce, #5e87c6, #493488)' }}
+        >
+          <h2 className="text-center text-3xl font-extrabold text-gray-900 mb-5">Login</h2>
           <div className="space-y-6">
             <div>
-              <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+              <label htmlFor="email" className="block text-sm font-bold text-black text-center">
                 Email
               </label>
               <div className="mt-1">
@@ -67,13 +63,13 @@ function Login() {
                   autoComplete="email"
                   onChange={(event) => setRecord({ ...record, email: event.target.value })}
                   required
-                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 />
               </div>
             </div>
 
             <div>
-              <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+              <label htmlFor="password" className="block text-sm font-bold text-black text-center">
                 Mot de passe
               </label>
               <div className="mt-1">
@@ -84,7 +80,7 @@ function Login() {
                   autoComplete="current-password"
                   onChange={(event) => setRecord({ ...record, password: event.target.value })}
                   required
-                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 />
               </div>
             </div>
@@ -93,14 +89,14 @@ function Login() {
                 <span className="block sm:inline">{error}</span>
               </div>
             )}
-
             <div>
               <button
                 onClick={(event) => {
                   event.preventDefault();
                   handleLogin();
                 }}
-                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                className="w-full flex justify-center py-2 px-4 rounded-md shadow-sm text-sm font-bold text-black bg-indigo-600 hover:bg-indigo-700"
+                style={{ backgroundColor: '#6bb5ce' }}
               >
                 Se connecter
               </button>
