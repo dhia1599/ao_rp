88786a56d3c360faeae7f699928057e89f046554
BenkhaddaMd <benkhada111@gmail.com>
2024-05-16 21:40:25 +0200

    Page Client with API request

--- frontend/src/pages/home/Client.tsx
+++ frontend/src/pages/home/Client.tsx
@@ -1,7 +1,32 @@
-import { Fragment } from 'react';
+import React, { useEffect, useState } from 'react';
+import { getAllClients } from '../../services/otherApi';
+
+export interface Client {
+    id: number;
+    email: string;
+    firstName: string;
+    lastName: string;
+  }
 
 function Client() {
-  return <Fragment>Client</Fragment>;
+
+    const [clients, setClients] = useState<Client[]>();
+
+    useEffect(() => {
+        getAllClients().then((res) => {
+            setClients(res.data)   
+            console.log(res.data);
+        })
+    }, []);
+
+  return (
+    <div>
+        <h1>List of Clients</h1>
+        {clients?.map((client) => (
+            <div>{client.email}</div>
+        ))}
+    </div>
+  )
 }
 
 export default Client;
